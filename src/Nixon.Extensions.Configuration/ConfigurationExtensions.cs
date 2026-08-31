8ddb975e2e04e20f35502448836244e3e490d844
Joshua Nixon <nixonjoshua98@gmail.com>
2026-08-31 09:55:57 +0100

    Bump Openiddict + open configuration package to net8 and net9

--- src/Nixon.Extensions.Configuration/ConfigurationExtensions.cs
+++ src/Nixon.Extensions.Configuration/ConfigurationExtensions.cs
@@ -6,43 +6,40 @@ namespace Nixon.Extensions.Configuration;
 
 public static class ConfigurationExtensions
 {
-    extension(IConfiguration configuration)
+    public static T GetRequiredSection<T>(this IConfiguration configuration, string sectionName) where T : class, new()
     {
-        public T GetRequiredSection<T>(string sectionName) where T : class, new()
-        {
-            T instance = new();
+        T instance = new();
 
-            var section = configuration.GetRequiredSection(sectionName);
+        var section = configuration.GetRequiredSection(sectionName);
 
-            section.Bind(instance);
+        section.Bind(instance);
 
-            return instance;
-        }
-
-        public bool TryGetValue<T>(string key, [MaybeNullWhen(false)] out T value)
-            where T : IParsable<T>
-        {
-            return T.TryParse(configuration[key], CultureInfo.InvariantCulture, out value);
-        }
-
-        public T GetValueOrDefault<T>(string key, T defaultValue = default!) where T : IParsable<T>
-        {
-            var value = configuration[key];
-
-            if (string.IsNullOrEmpty(value))
-            {
-                return defaultValue;
-            }
-
-            return T.TryParse(value, CultureInfo.InvariantCulture, out var parsedValue) ? 
-                parsedValue : 
-                throw new Exception($"Configuration key '{key}' was found but failed to be parsed");
-        }
-
-        public string GetRequiredConnectionString(string key) => 
-            configuration.GetRequiredValue($"ConnectionStrings:{key}");
-
-        public string GetRequiredValue(string key) =>
-            configuration[key] ?? throw new Exception($"Configuration key missing '{key}'");
+        return instance;
     }
+
+    public static bool TryGetValue<T>(this IConfiguration configuration, string key, [MaybeNullWhen(false)] out T value)
+        where T : IParsable<T>
+    {
+        return T.TryParse(configuration[key], CultureInfo.InvariantCulture, out value);
+    }
+
+    public static T GetValueOrDefault<T>(this IConfiguration configuration, string key, T defaultValue = default!) where T : IParsable<T>
+    {
+        var value = configuration[key];
+
+        if (string.IsNullOrEmpty(value))
+        {
+            return defaultValue;
+        }
+
+        return T.TryParse(value, CultureInfo.InvariantCulture, out var parsedValue) ? 
+            parsedValue : 
+            throw new Exception($"Configuration key '{key}' was found but failed to be parsed");
+    }
+
+    public static string GetRequiredConnectionString(this IConfiguration configuration, string key) => 
+        configuration.GetRequiredValue($"ConnectionStrings:{key}");
+
+    public static string GetRequiredValue(this IConfiguration configuration, string key) =>
+        configuration[key] ?? throw new Exception($"Configuration key missing '{key}'");
 }
